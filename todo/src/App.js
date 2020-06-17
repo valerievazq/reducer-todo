0f511048169afc4742a20f2ee84036943870854e
valerie-vazquez <vnvazquez@yahoo.com>
2020-06-16 19:26:38 -0600

    Redux

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {reducers} from "../reducers";
+function App() {
+  return (
+    <div className="App">
+      <Reducer />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo/src/reducers/Reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589,
+};
+
+export default Reducer = (state = initialState) => {
+  return state;
+};
